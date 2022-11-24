9a6118c704e4feffbe2f8775a1164257259295fa
Zoinks <murarashuvlad@gmail.com>
2022-11-24 21:30:35 +0200

    added gui

--- Tema3/main.py
+++ Tema3/main.py
@@ -1,11 +1,20 @@
-from Grammar import Grammar
+import dearpygui.dearpygui as dpg
 
 
-def main():
-    grammar = Grammar()
-    print("Tema 3")
-    grammar.print()
+def save_callback():
+    print("Save Clicked")
 
 
-if __name__ == '__main__':
-    main()
+dpg.create_context()
+dpg.create_viewport()
+dpg.setup_dearpygui()
+
+with dpg.window(label="Example Window"):
+    dpg.add_text("Hello world")
+    dpg.add_button(label="Save", callback=save_callback)
+    dpg.add_input_text(label="string")
+    dpg.add_slider_float(label="float")
+
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()

c2a4a2461daa1bf420fc5eb09ae0550462bbc305
danielmocanu38 <daniel-m.mocanu@student.unitbv.ro>
2022-11-24 18:23:09 +0200

    added regex_to_finite_automaton.

--- Tema1/main.py
+++ Tema1/main.py
@@ -1,9 +1,9 @@
-from DeterministicFiniteAutomaton import DeterministicFiniteAutomaton
-
-
+import re
+from regex_to_finite_automaton import regex_to_finite_automaton as R2FA
+from DeterministicFiniteAutomaton import DeterministicFiniteAutomaton as DFA
 def main():
-    dfa = DeterministicFiniteAutomaton()
-    dfa.print_automaton()
+    a:DFA = R2FA("^The.*Spain$")
+    a.print_automaton()
 
 
 if __name__ == '__main__':
---
+++ Tema1/regex_to_finite_automaton.py
@@ -0,0 +1,5 @@
+import re
+from DeterministicFiniteAutomaton import DeterministicFiniteAutomaton as DFA
+
+def regex_to_finite_automaton(r:str)->DFA:
+    return DFA()

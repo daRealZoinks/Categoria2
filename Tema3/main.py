79c45fb33ee8cba80d01a63c35c46a1d74e589f8
Zoinks <murarashuvlad@gmail.com>
2022-11-26 12:05:22 +0200

    ive played a lil bit w the GUI package

--- Tema3/main.py
+++ Tema3/main.py
@@ -1,5 +1,9 @@
 import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
+import dearpygui_ext.themes as themes
 import random
+
+dpg.create_context()
 
 
 def button_pressed():
@@ -9,15 +13,30 @@ def button_pressed():
     dpg.set_viewport_clear_color([r, g, b, 255])
 
 
-dpg.create_context()
-dpg.create_viewport()
-dpg.setup_dearpygui()
+dpg.create_viewport(title="Tema3", width=1270, height=720)
 
-with dpg.window(label="Ni ba", width=500, height=500, pos=(100, 100), modal=True):
+dark_theme = themes.create_theme_imgui_dark()
+light_theme = themes.create_theme_imgui_light()
+
+dpg.bind_theme(dark_theme)
+
+dpg.setup_dearpygui()
+dpg.show_style_editor()
+demo.show_demo()
+
+
+with dpg.window(label="Ni ba", width=500, height=200, pos=(100, 100)):
     dpg.add_text("O waw")
     dpg.add_button(label="Uite un buton", callback=button_pressed)
     dpg.add_input_text(label="aci putem scrie")
     dpg.add_slider_float(label="de asta putem trage")
+
+with dpg.window(label="Fereastra", width=500, height=150, pos=(100, 100)):
+    dpg.add_button(label="Uite alt buton", callback=button_pressed)
+    dpg.add_text("Nuj dc am atatea butoane si ferestre")
+    dpg.add_input_text(label="aci putem scrie si mai mult")
+    dpg.add_slider_float(label="de asta putem trage si mai mult", max_value=100, default_value=50)
+
 
 dpg.show_viewport()
 dpg.start_dearpygui()

e53af65fcc884c4e258febce7a85279ae7353cb9
danielmocanu38 <daniel-m.mocanu@student.unitbv.ro>
2023-01-04 18:55:33 +0200

    modified something in the menu

--- Tema2/main.py
+++ Tema2/main.py
@@ -6,16 +6,17 @@ def main():
     g = Grammar()
     g.read_grammar("grammar.txt")  # gramatica de tipul 3
 
-    m = None
+    m = grammar_to_automaton(g)
 
     if g.verify_grammar() and g.is_idc():
         while True:
             print("1. Afisarea gramaticii")
-            print("2. Generarea unui numar n de cuvinte in gramatica G")
-            print("3. Obtinerea automatului echivalent cu G")
-            print("4. Verificarea daca un cuvant este sau nu acceptat de automatul obtinut")
+            print("2. Generarea unui numar cuvant in gramatica G")
+            print("3. Afisara rezultatului gramaticii simplificate")
+            print("4. Afisarea gramaticii in FNG")
             print("5. Generarea unui cuvant in G + verificarea daca e acceptat de catre automat")
-            print("6. Iesire")
+            print("6. Verificarea daca un cuvant citit de la tastatura e acceptat de automat")
+            print("7. Iesire")
 
             option = input("Introduceti optiunea: ")
 
@@ -23,38 +24,32 @@ def main():
                 print(g)
 
             elif option == "2":
-                n = int(input("Introduceti numarul de cuvinte: "))
-                words = []
-                while len(words) < n:
-                    word = g.generate_word()
-                    if word not in words:
-                        words.append(word)
-                    else:
-                        print(f"Cuvantul \"{word}\" deja exista")
-                print(words)
+                word = g.generate_word()
+                print(word)
 
             elif option == "3":
-                m = grammar_to_automaton(g)
-                if m is not None:
-                    print(m)
+                print(g.simplify())
 
             elif option == "4":
-                word = input("Introduceti cuvantul: ")
-                if m is None:
-                    print("Automatul nu a fost generat")
-                else:
-                    print(m.check_word(word))
+                print(g.to_fng())
 
             elif option == "5":
                 word = g.generate_word()
-                print(word)
-                if m is None:
-                    print("Automatul nu a fost generat")
+                if(m.check_word(word)):
+                    print("Cuvantul este acceptat de automat")
                 else:
-                    print(m.check_word(word))
+                    print("Cuvantul nu este acceptat de automat")
 
             elif option == "6":
+                word = input("Introduceti cuvantul: ")
+                if (m.check_word(word)):
+                    print("Cuvantul este acceptat de automat")
+                else:
+                    print("Cuvantul nu este acceptat de automat")
+
+            elif option == "7":
                 break
+
             else:
                 print("Optiune invalida")
     else:
